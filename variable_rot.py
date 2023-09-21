2146639a5aaa6ebb1e8ee2fc2866c079f611cf7c
Gill-Singh-A <amansg22@iitk.ac.in>
2023-09-21 12:26:00 +0530

    Restored Variable Rotation functionality

--- variable_rot.py
+++ variable_rot.py
@@ -8,14 +8,14 @@ Variable ROT
 if __name__ == "__main__":
     lower_alph = [chr(ascii_value) for ascii_value in range(97, 123)]
     upper_alph = [alph.upper() for alph in lower_alph]
-    for arg in argv[1:]:
-        for rot in range(26):
-            decoded_message = ""
-            for alph in arg:
-                if alph in lower_alph:
-                    decoded_message += lower_alph[(lower_alph.index(alph)+rot)%len(lower_alph)]
-                elif alph in upper_alph:
-                    decoded_message += upper_alph[(upper_alph.index(alph)+rot)%len(upper_alph)]
-                else:
-                    decoded_message += alph
-            print(f"{Fore.CYAN}{rot}{Fore.RESET} : {Fore.YELLOW}{arg}{Fore.RESET} => {Fore.GREEN}{Style.BRIGHT}{decoded_message}{Fore.RESET}{Style.RESET_ALL}")
+    rot = int(argv[1])
+    for arg in argv[2:]:
+        decoded_message = ""
+        for alph in arg:
+            if alph in lower_alph:
+                decoded_message += lower_alph[(lower_alph.index(alph)+rot)%len(lower_alph)]
+            elif alph in upper_alph:
+                decoded_message += upper_alph[(upper_alph.index(alph)+rot)%len(upper_alph)]
+            else:
+                decoded_message += alph
+        print(f"{Fore.YELLOW}{arg}{Fore.RESET} => {Fore.GREEN}{Style.BRIGHT}{decoded_message}{Fore.RESET}{Style.RESET_ALL}")
